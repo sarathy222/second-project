674277b647bc2bfac1ff1de59c6b5a20036c54c4
sarathy222 <sharathy222@gmail.com>
2024-03-10 11:27:42 +0400

    first commit for install react js and tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import  {Home}  from "./pages/Home";
+import About  from "./pages/About";
+import  Profile from "./pages/Profile";
+import  SignIn  from "./pages/SignIn";
+import  SignUp  from "./pages/SignUp";
+export default function App() {
+  return(
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
